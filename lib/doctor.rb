a1bc850bb46893b590d3ea3d1289e72061bbc0bd
Ruel Tingling <rvtingling@gmail.com>
2019-11-11 15:04:54 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|appointment|
+      appointment.doctor == self
+    }
+  end
+
+  def new_appointment(patient,date)
+    Appointment.new(date,patient,self)
+  end
+
+  def patients
+    appointments.map { |appointment|
+      appointment.patient
+    }
+  end
+end
